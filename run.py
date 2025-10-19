fc4d28417996c57f4fb8a70147a341c895cfe511
Yusuke Kago <kago.y.955e@m.isct.ac.jp>
2025-10-19 16:08:41 +0900

    Fix import path for Flask_main on Render

--- run.py
+++ run.py
@@ -1,12 +1,7 @@
 # 実行
 # 開発・本番の両方を起動
-import sys
-import os
-
-# run.py のあるディレクトリを基準に app をパスに追加
-sys.path.append(os.path.join(os.path.dirname(__file__), "app"))
-
-from Flask_main import app  # これで Flask_main.py が見えるはず
+from app.Flask_main import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
