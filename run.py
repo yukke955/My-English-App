1efe332ffdcd554b2c9d9e934340abbeb62456bd
Yusuke Kago <kago.y.955e@m.isct.ac.jp>
2025-10-19 14:59:04 +0900

    Fix import path for Render

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 # 実行
 # 開発・本番の両方を起動
-from app import Flask_main
-import os
+from app.Flask_main import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render が指定するポート
-    Flask_main.app.run(host="0.0.0.0", port=port, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
